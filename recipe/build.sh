bb968d6bc5b2b43ba55fe462ffb200b76a7898c7
v-turin <vladimir.turin@opencascade.com>
2018-11-22 16:29:30 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 mkdir build && cd build
 
-cmake -G "Ninja"                            \
+#cmake -G "Ninja"                            \
+cmake                                       \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}        \
     -DCMAKE_BUILD_TYPE=Release              \
     -DCMAKE_INSTALL_LIBDIR=lib              \
     ..
 
-ninja install
+#ninja install
+make install
